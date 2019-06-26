eb08f557eb636f3dc780c76d2146246c43df7790
Ajay Mishra <ajaymishra@synsoftglobal.com>
2019-06-26 14:48:51 +0530

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,50 @@
-import React from "react"
+import React, {
+  Component
+} from "react"
 import { Link } from "gatsby"
-
+import QrReader from 'react-qr-reader'
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class Test extends Component {
+  state = {
+    result: 'No result'
+  }
 
-export default IndexPage
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+	return (
+		<Layout>
+		  <SEO title="Home" />
+		  <h1>Hi people</h1>
+		  <p>Welcome to your new Gatsby site.</p>
+		  <p>Now go build something great.</p>
+		  <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+			<Image />
+		  </div>
+		   <div>
+			<QrReader
+			  delay={300}
+			  onError={this.handleError}
+			  onScan={this.handleScan}
+			  style={{ width: '100%' }}
+			/>
+			<p>{this.state.result}</p>
+		  </div>
+		  <Link to="/page-2/">Go to page 2</Link>
+		</Layout>
+	)
+  }
+}
+
+export default Test

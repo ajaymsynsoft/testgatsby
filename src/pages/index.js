d5d2045ae78996bc57f8bef304d3ac66d6967d36
Ajay Mishra <ajaymishra@synsoftglobal.com>
2019-07-02 11:58:07 +0530

    push code

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,11 @@ import audioList from "../components/config"
 const queryString = require('query-string');
 var QrReader = '';
 
-class Test extends Component {
+class IndexPage extends Component {
+    /**
+        * Manage State For player and QR reader.        
+    */
     state = {
-        result: 'No result',
         QrReader: false,
         showQrReader: true,
         showMP3Player: false,
@@ -28,48 +30,55 @@ class Test extends Component {
         playbackRate: 1.0,
         loop: false
     }
-
+    /**
+        * Load component after page ready.
+        * QR code reader
+    */
     componentDidMount() {
-        console.log("componentDidMount",this.props.location.search);
-		var parsed = queryString.parse(this.props.location.search);		
-		console.log(parsed.pwa);		
-		console.log("navigator.onLine",navigator.onLine);		
-        try {	
-			if(navigator.onLine){
-				audioList.map(r=>{
-					console.log(r);
-					const audio = new Audio();
-					audio.src = r;					
-				});	
-				
-			}
+        var parsed = queryString.parse(this.props.location.search);
+        try {
+            if (navigator.onLine) {
+                audioList.map(r => {                   
+                    const audio = new Audio();
+                    audio.src = r;
+                    return r;
+                });
+
+            }
             QrReader = require("react-qr-reader");
-			if(parsed && !!parsed.pwa){
-				this.setState({ showWelcomeScreen: false });
-			}
+            if (parsed && !!parsed.pwa) {
+                this.setState({ showWelcomeScreen: false });
+            }
             this.setState({ QrReader: true });
-            console.log("QrReader", QrReader);
         } catch (e) {
             console.error(e);
         }
     }
 
-
+    /**
+        * Play and Pause event.        
+    */
     playPause = () => {
         this.setState({ playing: !this.state.playing })
     }
 
+    /**
+        * After whole played audio and stop button open QR reader view.        
+    */
+
     handleAfterEnd = data => {
-        console.log("handleAfterEnd", data);
         this.setState({
             url: '',
-            result: '',
             showQrReader: true,
             showMP3Player: false,
-			showWelcomeScreen: false,
+            showWelcomeScreen: false,
             playing: false
         })
     }
+
+    /**
+        * Restart Audio player .        
+    */
 
     handleRestart = data => {
         this.setState({ seeking: false })
@@ -77,47 +86,16 @@ class Test extends Component {
         this.setState({ seeking: true })
     }
 
+    /**
+        * Handle Scan QR code and if match with config file play audio.        
+    */
+
     handleScan = url => {
         console.log(url);
         if (typeof url == 'string') {
             url = url.toLowerCase();
-            console.log(url);
             if (audioList.indexOf(url) > -1) {
                 this.setState({
-                    result: url,
-                    showQrReader: false,
-                    showMP3Player: true,
-                    playing: true,
-                    url,
-                    played: 0,
-                    loaded: 0,
-                    pip: false
-                })       
-
-
-
-            }
-        }
-    }
-
-
-
-    ref = player => {
-        this.player = player;
-
-    }
-
-    handleLoad = url => {
-        console.log(url);
-		//alert("Welcome Screen");
-        // alert(JSON.stringify(data));
-        if (typeof url == 'string') {
-            url = url.toLowerCase();
-            console.log("url", audioList.indexOf(url));
-            console.log(url);
-            if (audioList.indexOf(url) > -1) {
-                this.setState({
-                   result: url,
                     showQrReader: false,
                     showMP3Player: true,
                     playing: true,
@@ -130,9 +108,39 @@ class Test extends Component {
         }
     }
 
+
+
+    ref = player => {
+        this.player = player;
+
+    }
+
+    /**
+        * Load QR code reader and play audio file.        
+    */
+
+    handleLoad = url => {
+        if (typeof url == 'string') {
+            url = url.toLowerCase();
+            if (audioList.indexOf(url) > -1) {
+                this.setState({
+                    showQrReader: false,
+                    showMP3Player: true,
+                    playing: true,
+                    url,
+                    played: 0,
+                    loaded: 0,
+                    pip: false
+                })
+            }
+        }
+    }
+
+    /**
+        * Set state on progress playing audio file.        
+    */
+
     onProgress = state => {
-        console.log('onProgress', state)
-        // We only want to update time slider if we are not currently seeking
         if (!this.state.seeking) {
             this.setState(state)
         }
@@ -144,6 +152,7 @@ class Test extends Component {
     onSeekChange = e => {
         this.setState({ played: parseFloat(e.target.value) })
     }
+
     onSeekMouseUp = e => {
         this.setState({ seeking: false })
         this.player.seekTo(parseFloat(e.target.value))
@@ -160,86 +169,88 @@ class Test extends Component {
     }
 
 
-
     handleError = err => {
-        console.error(JSON.stringify(err));
         console.error(err)
     }
-	
-	onError = err => {
+
+    /**
+        * Handle Error while playing audio file.     
+    */
+
+    onError = err => {
         alert("You are offline. For this QR code please go online.");
-        console.error(err);
-		this.setState({
+        this.setState({
             url: '',
-            result: '',
             showQrReader: true,
             showMP3Player: false,
-			showWelcomeScreen: false,
+            showWelcomeScreen: false,
             playing: false
         })
     }
 
-
+    /**
+        * Render camera and player view.     
+    */
     render() {
         const { url, playing, controls, muted } = this.state
         return (
             <Layout>  
-		{ this.state.showWelcomeScreen  &&    
-           <div
-			  style={{
-				margin: `0 auto`,
-				maxWidth: 960,
-				padding: `1.45rem 1.0875rem`,
-			  }}
-			>
-			<h1>Welcome.....</h1>
-			<p>Please  <button className="button" onClick={this.handleAfterEnd}>click here</button> to scan QR code.</p>
-          </div>         
-        }			
-        { !this.state.showWelcomeScreen && this.state.showQrReader && this.state.QrReader &&    
-          <div className='qrcode-wrapper'>
-         <QrReader
-          delay={300}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          onLoad={this.handleLoad}
-          style={{ width: '100%' }}
-          />  
-          </div>         
-        }
+                { this.state.showWelcomeScreen  &&    
+                   <div
+                      style={{
+                        margin: `0 auto`,
+                        maxWidth: 960,
+                        padding: `1.45rem 1.0875rem`,
+                      }}
+                    >
+                    <h1>Welcome.....</h1>
+                    <p>Please  <button className="button" onClick={this.handleAfterEnd}>click here</button> to scan QR code.</p>
+                  </div>         
+                }           
+                { !this.state.showWelcomeScreen && this.state.showQrReader && this.state.QrReader &&    
+                  <div className='qrcode-wrapper'>
+                 <QrReader
+                  delay={300}
+                  onError={this.handleError}
+                  onScan={this.handleScan}
+                  onLoad={this.handleLoad}
+                  style={{ width: '100%' }}
+                  />  
+                  </div>         
+                }
 
-        { !this.state.showWelcomeScreen && this.state.showMP3Player && 
-          <>
-          <div className='player-wrapper'>
-          <ReactPlayer 
-            width='100%'
-            height='100%'
-            autoPlay
-            ref={this.ref}
-            url={url}     
-            onEnded={this.handleAfterEnd}           
-            playing={playing}       
-            muted={muted}       
-            controls={controls}                    
-            onPlay={this.onPlay}             
-            onPause={this.onPause}
-            onProgress={this.onProgress}          
-            onError={this.onError}          
-                
-          />    
-          <div className="audioButton"> 
-           <button className="button" onClick={this.handleAfterEnd}>Back</button>
-            <button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</button>
-            <button className="button" onClick={this.handleAfterEnd}>Stop</button>
-            <button className="button" onClick={this.handleRestart}>Re-start</button>
-            </div>
-          </div>
-           
-          </>
-        }		
-        </Layout>
+                { !this.state.showWelcomeScreen && this.state.showMP3Player && 
+                  <>
+                  <div className='player-wrapper'>
+                  <ReactPlayer 
+                    width='100%'
+                    height='100%'
+                    autoPlay
+                    ref={this.ref}
+                    url={url}     
+                    onEnded={this.handleAfterEnd}           
+                    playing={playing}       
+                    muted={muted}       
+                    controls={controls}                    
+                    onPlay={this.onPlay}             
+                    onPause={this.onPause}
+                    onProgress={this.onProgress}          
+                    onError={this.onError}          
+                        
+                  />    
+                  <div className="audioButton"> 
+                   <button className="button" onClick={this.handleAfterEnd}>Back</button>
+                    <button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</button>
+                    <button className="button" onClick={this.handleAfterEnd}>Stop</button>
+                    <button className="button" onClick={this.handleRestart}>Re-start</button>
+                    </div>
+                  </div>
+                   
+                  </>
+                }       
+            </Layout>
         )
     }
 }
 
-export default Test
+export default IndexPage
